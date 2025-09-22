7209da9b43212fd1475f4171ac2ec5db154a2345
Yehor Nazarets <nazarets1111@gmail.com>
2025-09-22 14:20:25 +0200

    added mvn dependency for Hb-Validation and added annotation for validation data

--- src/main/java/mate/academy/app/controller/BookController.java
+++ src/main/java/mate/academy/app/controller/BookController.java
@@ -1,5 +1,6 @@
 package mate.academy.app.controller;
 
+import jakarta.validation.Valid;
 import java.util.List;
 import lombok.RequiredArgsConstructor;
 import mate.academy.app.dto.BookDto;
@@ -29,7 +30,7 @@ public class BookController {
 
     @PostMapping
     @ResponseStatus(HttpStatus.CREATED)
-    public BookDto createBook(@RequestBody CreateBookRequestDto requestDto) {
+    public BookDto createBook(@RequestBody @Valid CreateBookRequestDto requestDto) {
         return bookService.save(requestDto);
     }
 
--- src/main/java/mate/academy/app/dto/CreateBookRequestDto.java
+++ src/main/java/mate/academy/app/dto/CreateBookRequestDto.java
@@ -1,5 +1,7 @@
 package mate.academy.app.dto;
 
+import jakarta.validation.constraints.Min;
+import jakarta.validation.constraints.NotNull;
 import java.math.BigDecimal;
 import lombok.Getter;
 import lombok.Setter;
@@ -7,9 +9,14 @@ import lombok.Setter;
 @Getter
 @Setter
 public class CreateBookRequestDto {
+    @NotNull
     private String title;
+    @NotNull
     private String author;
+    @NotNull
     private String isbn;
+    @NotNull
+    @Min(0)
     private BigDecimal price;
     private String description;
     private String coverImage;
