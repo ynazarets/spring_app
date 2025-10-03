a58b779711cf2283189454430f3be7f9dbf7fbd1
Yehor Nazarets <nazarets1111@gmail.com>
2025-10-03 18:57:23 +0200

    fixed securitycnfg, added new endpoints

--- src/main/java/mate/academy/app/config/SecurityConfig.java
+++ src/main/java/mate/academy/app/config/SecurityConfig.java
@@ -31,7 +31,7 @@ public class SecurityConfig {
                 .cors(AbstractHttpConfigurer::disable)
                 .csrf(AbstractHttpConfigurer::disable)
                 .authorizeHttpRequests(auth -> auth
-                        .requestMatchers("/auth/**")
+                        .requestMatchers("/auth/**", "/swagger-ui/**", "/v3/api-docs/**")
                         .permitAll()
                         .anyRequest()
                         .authenticated())
