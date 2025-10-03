2e8750b61bf6abba1bcc28f0f6388564d1a50416
Yehor Nazarets <nazarets1111@gmail.com>
2025-10-03 17:46:28 +0200

    inserted admin to db

--- src/main/java/mate/academy/app/service/impl/UserServiceImpl.java
+++ src/main/java/mate/academy/app/service/impl/UserServiceImpl.java
@@ -16,19 +16,19 @@ public class UserServiceImpl implements UserService {
     private final UserRepository userRepository;
     private final UserMapper userMapper;
 
-    @Override
-    public UserResponseDto registerUser(UserRegistrationRequestDto requestDto)
-            throws RegistrationException {
-        if (userRepository.findByEmail(requestDto.getEmail()).isPresent()) {
-            throw new RegistrationException("Can not create a new User, "
-                    + "this email already exist");
+        @Override
+        public UserResponseDto registerUser(UserRegistrationRequestDto requestDto)
+                throws RegistrationException {
+            if (userRepository.findByEmail(requestDto.getEmail()).isPresent()) {
+                throw new RegistrationException("Can not create a new User, "
+                        + "this email already exist");
+            }
+            User user = new User();
+            user.setEmail(requestDto.getEmail());
+            user.setPassword(requestDto.getPassword());
+            user.setFirstName(requestDto.getFirstName());
+            user.setLastName(requestDto.getLastName());
+            user.setShippingAddress(requestDto.getShippingAddress());
+            return userMapper.toUserResponseDto(userRepository.save(user));
         }
-        User user = new User();
-        user.setEmail(requestDto.getEmail());
-        user.setPassword(requestDto.getPassword());
-        user.setFirstName(requestDto.getFirstName());
-        user.setLastName(requestDto.getLastName());
-        user.setShippingAddress(requestDto.getShippingAddress());
-        return userMapper.toUserResponseDto(userRepository.save(user));
     }
-}
