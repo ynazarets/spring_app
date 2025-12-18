a9e5f3287038bf0f4228e633c8baa0574f767de1
Yehor Nazarets <nazarets1111@gmail.com>
2025-12-18 01:00:09 +0100

    added sql files and scripts, and test for bookController

--- src/test/java/mate/academy/app/controller/BookControllerTest.java
+++ src/test/java/mate/academy/app/controller/BookControllerTest.java
@@ -1,4 +1,117 @@
 package mate.academy.app.controller;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
+import lombok.SneakyThrows;
+import mate.academy.app.dto.book.BookDto;
+import mate.academy.app.dto.book.CreateBookRequestDto;
+import org.junit.jupiter.api.AfterAll;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.core.io.ClassPathResource;
+import org.springframework.http.MediaType;
+import org.springframework.security.test.context.support.WithMockUser;
+import org.springframework.test.context.jdbc.Sql;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.MvcResult;
+import org.springframework.test.web.servlet.setup.MockMvcBuilders;
+import org.springframework.web.context.WebApplicationContext;
+import org.springframework.jdbc.datasource.init.ScriptUtils;
+import org.testcontainers.shaded.org.apache.commons.lang3.builder.EqualsBuilder;
+
+import javax.sql.DataSource;
+import java.math.BigDecimal;
+import java.sql.Connection;
+import java.sql.SQLException;
+
+import static org.springframework.security.test.web.servlet.setup.SecurityMockMvcConfigurers.springSecurity;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 public class BookControllerTest {
+
+    protected static MockMvc mockMvc;
+    @Autowired
+    private ObjectMapper objectMapper;
+
+    @BeforeAll
+    static void beforeAll(
+            @Autowired WebApplicationContext applicationContext,
+            @Autowired DataSource dataSource
+            ) throws SQLException {
+        mockMvc = MockMvcBuilders
+                .webAppContextSetup(applicationContext)
+                .apply(springSecurity())
+                .build();
+        teardown(dataSource);
+        try (Connection connection = dataSource.getConnection()) {
+            connection.setAutoCommit(true);
+            ScriptUtils.executeSqlScript(
+                    connection,
+                    new ClassPathResource("database/books/add-three-default-books.sql",
+                            BookControllerTest.class.getClassLoader())
+            );
+        }
+    }
+
+    @AfterAll
+    static void afterAll(
+            @Autowired DataSource dataSource
+    ) {
+        teardown(dataSource);
+    }
+
+    @SneakyThrows
+    private static void teardown(DataSource dataSource) {
+        try (Connection connection = dataSource.getConnection()) {
+            connection.setAutoCommit(true);
+            ScriptUtils.executeSqlScript(
+                    connection,
+                    new ClassPathResource("database/books/remove-all-books.sql",
+                            BookControllerTest.class.getClassLoader())
+            );
+        }
+    }
+
+    @WithMockUser(username = "admin", roles = {"ADMIN"})
+    @Test
+    @DisplayName("""
+            da
+            """)
+    @Sql(
+            scripts = "classpath:database/books/delete-test-book.sql",
+            executionPhase = Sql.ExecutionPhase.AFTER_TEST_METHOD
+    )
+    public void createBook_ValidRequest_ShouldCreateBook() throws Exception {
+        CreateBookRequestDto requestDto = new CreateBookRequestDto();
+        requestDto.setTitle("TitleTest");
+        requestDto.setAuthor("AuthorTest");
+        requestDto.setIsbn("123456789");
+        requestDto.setPrice(BigDecimal.valueOf(10.35));
+
+        BookDto expectedDto = new BookDto();
+        expectedDto.setTitle(requestDto.getTitle());
+        expectedDto.setAuthor(requestDto.getAuthor());
+        expectedDto.setIsbn(requestDto.getIsbn());
+        expectedDto.setPrice(requestDto.getPrice());
+
+        String jsonRequest = objectMapper.writeValueAsString(requestDto);
+
+        MvcResult result = mockMvc.perform(
+                post("/books")
+                .content(jsonRequest)
+                .contentType(MediaType.APPLICATION_JSON)
+                )
+                .andExpect(status().isCreated())
+                .andReturn();
+
+        BookDto actualDto = objectMapper.readValue(result.getResponse().getContentAsString(), BookDto.class);
+        Assertions.assertNotNull(actualDto);
+        Assertions.assertNotNull(actualDto.getId());
+        EqualsBuilder.reflectionEquals(expectedDto, actualDto, "id");
+    }
 }
---
+++ src/test/resources/database/books/delete-test-book.sql
@@ -0,0 +1,1 @@
+delete from books where title = 'TitleTest';
