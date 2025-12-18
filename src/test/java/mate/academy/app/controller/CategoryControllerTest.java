dbc05e829155e3584be11d8a4803ece2969dc21b
Yehor Nazarets <nazarets1111@gmail.com>
2025-12-18 17:36:41 +0100

    added tests for CategoryController

--- src/test/java/mate/academy/app/controller/CategoryControllerTest.java
+++ src/test/java/mate/academy/app/controller/CategoryControllerTest.java
@@ -1,9 +1,42 @@
 package mate.academy.app.controller;
 
+import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
+import lombok.SneakyThrows;
+import mate.academy.app.dto.book.BookDto;
+import mate.academy.app.dto.category.CategoryDto;
+import mate.academy.app.dto.category.CreateCategoryRequestDto;
+import org.junit.jupiter.api.AfterAll;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.core.io.ClassPathResource;
+import org.springframework.http.MediaType;
+import org.springframework.jdbc.datasource.init.ScriptUtils;
+import org.springframework.security.test.context.support.WithMockUser;
+import org.springframework.test.context.jdbc.Sql;
 import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.MvcResult;
+import org.springframework.test.web.servlet.setup.MockMvcBuilders;
+import org.springframework.web.context.WebApplicationContext;
+import org.testcontainers.shaded.org.apache.commons.lang3.builder.EqualsBuilder;
+
+import javax.sql.DataSource;
+import java.io.IOException;
+import java.io.UnsupportedEncodingException;
+import java.sql.Connection;
+import java.sql.SQLException;
+import java.util.List;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNotNull;
+import static org.springframework.security.test.web.servlet.setup.SecurityMockMvcConfigurers.springSecurity;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.put;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 public class CategoryControllerTest {
@@ -11,4 +44,156 @@ public class CategoryControllerTest {
     @Autowired
     private ObjectMapper objectMapper;
 
+    @BeforeAll
+    static void beforeAll(
+            @Autowired WebApplicationContext applicationContext,
+            @Autowired DataSource dataSource
+    ) throws SQLException {
+        mockMvc = MockMvcBuilders
+                .webAppContextSetup(applicationContext)
+                .apply(springSecurity())
+                .build();
+        teardown(dataSource);
+        try (Connection connection = dataSource.getConnection()) {
+            connection.setAutoCommit(true);
+            ScriptUtils.executeSqlScript(
+                    connection,
+                    new ClassPathResource("database/categories/add-three-default-categories.sql",
+                            BookControllerTest.class.getClassLoader())
+            );
+        }
+    }
+
+    @AfterAll
+    static void afterAll(
+            @Autowired DataSource dataSource
+    ) {
+        teardown(dataSource);
+    }
+
+    @SneakyThrows
+    private static void teardown(DataSource dataSource) {
+        try (Connection connection = dataSource.getConnection()) {
+            connection.setAutoCommit(true);
+            ScriptUtils.executeSqlScript(
+                    connection,
+                    new ClassPathResource("database/categories/remove-all-categories.sql",
+                            BookControllerTest.class.getClassLoader())
+            );
+        }
+    }
+
+    @WithMockUser(username = "admin", roles = {"ADMIN"})
+    @Test
+    @DisplayName("""
+            da
+            """)
+    @Sql(
+            scripts = "classpath:database/categories/delete-test-category.sql",
+            executionPhase = Sql.ExecutionPhase.AFTER_TEST_METHOD
+    )
+    public void createCategory_ValidCategoryDto_ShouldSaveCategory() throws Exception {
+        CreateCategoryRequestDto requestDto = new CreateCategoryRequestDto();
+        requestDto.setName("NameTest");
+
+        CategoryDto expectedDto = new CategoryDto();
+        expectedDto.setName(requestDto.getName());
+
+        String jsonRequest = objectMapper.writeValueAsString(requestDto);
+
+        MvcResult result = mockMvc.perform(
+                        post("/categories")
+                                .content(jsonRequest)
+                                .contentType(MediaType.APPLICATION_JSON)
+                )
+                .andExpect(status().isCreated())
+                .andReturn();
+
+        CategoryDto actual = objectMapper.readValue(result.getResponse().getContentAsString(),
+                CategoryDto.class);
+
+        assertNotNull(actual);
+        assertNotNull(actual.getId());
+        EqualsBuilder.reflectionEquals(expectedDto, actual, "id");
+    }
+
+    @Test
+    @DisplayName("""
+            dLLLa
+            """)
+    public void getAll_validRequest_ShouldReturnPageOfCategoryDto() throws Exception {
+        MvcResult result = mockMvc.perform(
+                        get("/categories")
+                                .param("page", "0")
+                                .param("size", "10")
+                                .param("sort", "name,asc")
+                                .contentType(MediaType.APPLICATION_JSON)
+                )
+                .andExpect(status().isOk())
+                .andReturn();
+        String jsonResponse = result.getResponse().getContentAsString();
+
+        com.fasterxml.jackson.databind.JsonNode jsonNode = objectMapper.readTree(jsonResponse);
+        com.fasterxml.jackson.databind.JsonNode contentNode = jsonNode.get("content");
+
+        List<CategoryDto> actual = objectMapper.readValue(contentNode.traverse(),
+                objectMapper.getTypeFactory().constructCollectionType(List.class, CategoryDto.class));
+        assertEquals(3, actual.size());
+        assertEquals("Category1", actual.get(0).getName());
+        assertNotNull(actual.get(2).getId());
+    }
+
+    @Test
+    @DisplayName("""
+            dBJa
+            """)
+    public void getById_ValidId_ShouldReturnCategoryDto() throws Exception {
+        Long RequestId = 1L;
+        CategoryDto expectedDto = new CategoryDto();
+        expectedDto.setId(RequestId);
+        expectedDto.setName("Category1");
+
+        MvcResult result = mockMvc.perform(
+                        get("/categories/" + RequestId)
+                                .contentType(MediaType.APPLICATION_JSON)
+                )
+                .andExpect(status().isOk())
+                .andReturn();
+
+        CategoryDto actualDto = objectMapper.readValue(
+                result.getResponse().getContentAsString(),
+                CategoryDto.class
+        );
+        assertNotNull(actualDto);
+        EqualsBuilder.reflectionEquals(expectedDto, actualDto, "id");
+    }
+
+    @WithMockUser(username = "admin", roles = {"ADMIN"})
+    @Test
+    @DisplayName("""
+            dBJa
+            """)
+    public void updateCategory_ValidId_ShouldUpdateCategory() throws Exception {
+        Long requestId = 1L;
+
+        CreateCategoryRequestDto requestDto = new CreateCategoryRequestDto();
+        requestDto.setName("TestName2");
+
+        String jsonRequest = objectMapper.writeValueAsString(requestDto);
+
+        MvcResult result = mockMvc.perform(
+                        put("/categories/" + requestId)
+                                .content(jsonRequest)
+                                .contentType(MediaType.APPLICATION_JSON)
+                )
+                .andExpect(status().isCreated())
+                .andReturn();
+
+        CategoryDto actualDto = objectMapper.readValue(
+                result.getResponse().getContentAsString(), CategoryDto.class
+        );
+
+        assertEquals("TestName2", actualDto.getName());
+        assertEquals(requestId, actualDto.getId());
+    }
 }
---
+++ src/test/resources/database/categories/add-three-default-categories.sql
@@ -0,0 +1,3 @@
+INSERT INTO category (id, name) VALUES (1,'Category1'), (2,'Category2'), (3,'Category3');
+
+ALTER TABLE category ALTER COLUMN id RESTART WITH 100;
